66ee0ff9801050988174acd681bc85ece9081237
barberousse <benoit@skillsesh.com>
2014-08-27 20:11:25 +0000

    Renamed linkin keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, Rails.application.secrets.facebook_id, Rails.application.secrets.facebook_secret, {
     image_size: {width: 500, height: 500}
   }
-  provider :linkedin, Rails.application.secrets.linked_in_id, Rails.application.secrets.linked_in_secret
+ provider :linkedin, Rails.application.secrets.linkedin_id, Rails.application.secrets.linkedin_secret
   provider :google_oauth2, Rails.application.secrets.google_id, Rails.application.secrets.google_secret, { name: 'google', image_size: 'square' }
   
 end
