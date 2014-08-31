5d5255e1444bee0c97509d12ff59b5a059e63d0a
TheAntiEgo <jt.connor88@gmail.com>
2014-08-31 06:01:58 +0000

    Corrected Google omniauth parameters

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,5 +3,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     image_size: {width: 500, height: 500}
   }
   provider :linkedin, ENV['LINKEDIN_ID'], ENV['LINKEDIN_SECRET']
-  provider :google_oauth2, ENV['GOOGLE_ID'], ENV['GOOGLE_SECRET'], { name: 'google', image_size: 'square' }
+  provider :google_oauth2, ENV['GOOGLE_ID'], ENV['GOOGLE_SECRET'], { name: 'google', image_aspect_ratio: 'square', image_size: '500' }
 end
