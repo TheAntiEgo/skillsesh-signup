9fc87f7bf9536a90346280d9cc4a61b01df24647
TheAntiEgo <jt.connor88@gmail.com>
2014-09-01 01:51:46 +0000

    Needless line

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,6 @@
               :email => Faker::Internet.email,
               :bio => Faker::Lorem.sentences(3).join(" "),
               :photo => "http://api.randomuser.me/portraits/" + %w{ men women}.shuffle.first + '/' + (0...95).last(94).shuffle.first.to_s + '.jpg',
-              :learnables => Faker::Lorem.words((2...5).last(3).shuffle.first).join(" "),
               :teachables => Faker::Lorem.words((2...5).last(3).shuffle.first).join(" ")
               )
 }
