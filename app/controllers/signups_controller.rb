d76fcf7aabf2afe7caf33e6de88306208ad36326
TheAntiEgo <jt.connor88@gmail.com>
2014-09-08 10:08:16 +0000

    Catch registered users (accidently) signing up twice

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -1,15 +1,21 @@
 class SignupsController < ApplicationController
+  rescue_from ActiveRecord::ActiveRecordError, :with => :catch_activerecord
+  
   def index
     @profiles = Profile.all.limit(15)
   end
   
   def create
-      @user = User.new( :email => get_auth[:info][:email] )
-      @user.authentications << Authentication.from_omniauth( get_auth )
-      @user.profile = Profile.from_omniauth( get_auth )
-      @user.save!
-      session[:remember_token] = @user.remember_token
-      redirect_to onboard_path
+    if session[:remember_token] && User.where(:remember_token => session[:remember_token])
+      redirect_to(root_path, :notice => "Why don't you share us with your friends instead?") 
+      return
+    end
+    @user = User.new( :email => get_auth[:info][:email], :remember_token => SecureRandom.uuid )
+    @user.authentications << Authentication.from_omniauth( get_auth )
+    @user.profile = Profile.from_omniauth( get_auth )
+    @user.save!
+    session[:remember_token] = @user.remember_token
+    redirect_to onboard_path
   end
   
   def onboard
@@ -22,12 +28,11 @@ class SignupsController < ApplicationController
   end
   
   def update
-    byebug
-    if request.method == 'patch'
-      current_user.profile.update(get_profile)
+    if params[:profile]
+      current_user.profile.update(params.require(:profile).permit(:bio, :skills))
       redirect_to first_session_path
     else
-      current_user.profile.courses << Course.new(get_course)
+      current_user.profile.courses << Course.new(params.require(:course).permit(:name, :goal, :how, :duration, :price, :location))
       current_user.save!
       redirect_to root_path, :notice => "That's it! All done!"
     end      
@@ -35,15 +40,13 @@ class SignupsController < ApplicationController
   
   private
   
-  def get_profile
-    params.require(:profile).permit(:bio, :skills)
-  end
-  
-  def get_course
-    params.require(:course).permit(:name, :goal, :how, :duration, :price, :location)
-  end
-  
   def get_auth
     request.env['omniauth.auth']
+  end
+  
+  def catch_activerecord(e)
+    logger.error e.exception.to_s
+    logger.error e.message.to_s
+    redirect_to root_url, :alert => "Something went wrong! Try again or contact us for help"
   end
 end
