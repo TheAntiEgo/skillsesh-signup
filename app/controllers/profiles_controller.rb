880aa4541cdb6fb6caa8d35e0ff579a78e535039
TheAntiEgo <jt.connor88@gmail.com>
2014-09-16 04:30:28 +0000

    Beast mode refactor of controller logic

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,21 +1,18 @@
 class ProfilesController < ApplicationController
-  before_action :current_user?
+  before_action :authenticated?, 	:current_user?
   
   def show
-    redirect_to root_path if !logged_in?
     @profile = @user.profile
-    render 'users/show'
   end
   
   def update
-    @current_user.profile.update(get_params)
-    redirect_to :action => :show, :notice => "Profile updated!"
+    @user.profile.update!(get_params)
   end
   
   private
   
   def get_params
-    params.require(:profile).permit(:first_name, :last_name, :photo, :bio)
+    params.permit(:profile => [:first_name, :last_name, :photo, :bio], :course => [:name, :goal, :how, :requirements, :duration, :price, :location, :skills])
   end    
   
 end
