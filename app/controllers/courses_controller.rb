79f6f46cb3ee840b18cfff0131afc090ad5f4c40
barberousse <benoit@skillsesh.com>
2014-09-24 03:22:05 +0000

    Snapshot of profile: List and successfully edit courses; fields for adding a new record but no submission functionality

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,9 +1,28 @@
 class CoursesController < ApplicationController
+  respond_to :json, :only => [:create, :update]
+  
   def index
    @courses = Course.where(:profile => current_user?.profile)
   end
   
   def show
     @course = Course.find(params[:id])
+  end
+  
+  def create
+    @course = Course.create!(get_params[:course])
+    render :json => @course
+  end
+  
+  def update
+    @course = Course.find(get_params[:id])
+    @course.update!(get_params[:course])
+    render :json => @course
+  end
+  
+  private
+  
+  def get_params
+    params.permit :id, :course => [:name, :goal, :how, :requirements, :duration, :price, :location, :skills]
   end
 end
