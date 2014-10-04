bb823ffdfae66205f5be764f760c449282f10d37
barberousse <benoit@skillsesh.com>
2014-10-04 02:33:39 +0000

    Added show method to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < ApplicationController
+  before_action :current_user?
+  
+  def show
+    @profile = User.find(params[:id]).profile
+    render :template => 'profiles/show'
+  end
 end
