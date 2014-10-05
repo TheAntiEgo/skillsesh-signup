b5d2071ef2300ffb86cb6685cab06d9d68de7e10
barberousse <benoit@skillsesh.com>
2014-10-05 02:29:20 +0000

    Migrated bare Schedule/Appointment models

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140915010354) do
+ActiveRecord::Schema.define(version: 20141005003317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,14 @@ ActiveRecord::Schema.define(version: 20140915010354) do
 
   add_index "profiles_skills", ["profile_id"], name: "index_profiles_skills_on_profile_id", using: :btree
   add_index "profiles_skills", ["skill_id"], name: "index_profiles_skills_on_skill_id", using: :btree
+
+  create_table "schedules", force: true do |t|
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "schedules", ["user_id"], name: "index_schedules_on_user_id", using: :btree
 
   create_table "skills", force: true do |t|
     t.string   "name",       limit: 20, null: false
