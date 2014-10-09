4227c501eba3eb8d04b148915c8aedb8528bf4c2
barberousse <benoit@skillsesh.com>
2014-10-09 02:06:40 +0000

    Can check Conversation record for who needs to be notified of update

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,4 +1,20 @@
 class Conversation < ActiveRecord::Base
   belongs_to :customer, class_name: "User"
   belongs_to :merchant, class_name: "User"
+  
+  ##
+  # Instance methods
+  ##
+  def is_unread_by
+    unless self.merchant_read_at.nil?
+      if self.customer_read_at < self.merchant_read_at
+        self.customer
+      else
+        self.merchant
+      end
+    else
+      self.merchant
+    end
+  end
+        
 end
