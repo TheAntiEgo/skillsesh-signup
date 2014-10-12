543f5b3a76978b51c6cb03b889b26c40b1e7a329
barberousse <benoit@skillsesh.com>
2014-10-12 04:51:39 +0000

    conversations controller handles input correctly now

---
+++ app/controllers/conversations_controller.rb
@@ -0,0 +1,26 @@
+class ConversationsController < ApplicationController
+  before_action :authenticated?
+  respond_to :json
+  
+  def create
+    byebug
+    convo_data = get_params[:conversation]
+    @convo = Conversation.new do |c|
+      c.subject = convo_data[:subject]
+      c.merchant = User.find(convo_data[:merchant])
+      c.customer = User.find(convo_data[:customer])
+      c.body = [convo_data[:body]]
+      c.customer_read_at = DateTime.now
+    end
+    @convo.save!
+    render :nothing => true
+  end
+
+  def update
+    render :nothing => true
+  end
+  
+  def get_params
+    params.permit(:conversation => [:subject, :merchant, :customer, :body])
+  end    
+end
