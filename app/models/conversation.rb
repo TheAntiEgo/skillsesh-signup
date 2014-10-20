1c3c4ae7780c456069b48e31c93519f1473c2f40
Benoît Barberousse <benoit@skillsesh.com>
2014-10-20 02:15:27 +0000

    Rewrite of messaging to be more atomic

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -17,6 +17,18 @@ class Conversation < ActiveRecord::Base
     end
   end
 
+  def customer_read_at
+    return self.messages[-2].created_at if self.messages.count.even? && self.messages.last.read_at.nil?
+    return self.messages.last.read_at if self.messages.count.even?
+    return self.messages.last.created if self.messages.count.odd?
+  end
+
+  def merchant_read_at
+    return self.messages[-2].created_at if self.messages.count.odd? && self.messages.last.read_at.nil?
+    return self.messages.last.read_at if self.messages.count.odd?
+    return self.messages.last.created_at if self.messages.even?
+  end
+
   def newest_first
     self.messages.to_ary.reverse
   end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,6 @@
 class Message < ActiveRecord::Base
   belongs_to :sender
   belongs_to :receiver
+  belongs_to :conversation, :inverse_of => :message
+  belongs_to :parent, :class_name => 'Message', :inverse_of => :child
 end
