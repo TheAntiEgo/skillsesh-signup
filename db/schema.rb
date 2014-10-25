01d2828cd0c0b97bf808908565f40db0b8922264
barberousse <benoit@skillsesh.com>
2014-10-25 20:26:46 +0000

    Set default read field value to false on Message

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141025195447) do
+ActiveRecord::Schema.define(version: 20141025202545) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,7 +72,7 @@ ActiveRecord::Schema.define(version: 20141025195447) do
     t.datetime "updated_at"
     t.integer  "conversation_id"
     t.integer  "parent_id"
-    t.boolean  "read"
+    t.boolean  "read",            default: false
   end
 
   add_index "messages", ["conversation_id"], name: "index_messages_on_conversation_id", using: :btree
