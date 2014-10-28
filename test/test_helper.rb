35e9122e0c00c7c8ffa6be08534e50787e5e7b3c
barberousse <benoit@skillsesh.com>
2014-10-28 21:20:06 +0000

    Re-add FactoryGirl methods to test cases

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,5 +7,5 @@ class ActiveSupport::TestCase
   fixtures :all
   
   # Add more helper methods to be used by all tests here...
-  include FactoryGirl::Syntax::Methods
+  include FactoryGirl::Syntax::Methods  
 end
