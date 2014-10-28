b629ee9bf303c34b0443febd9bc4240e241d68cb
barberousse <benoit@skillsesh.com>
2014-10-28 21:17:35 +0000

    Merge Profile and User models

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,13 +12,43 @@ class User < ActiveRecord::Base
   has_many :questions, class_name: 'Conversation', foreign_key: 'customer_id'
   has_many :answers, class_name: 'Conversation', foreign_key: 'merchant_id'
   has_many :messages
-  has_one :schedule, :inverse_of => :user
-  has_and_belongs_to_many :skills
+  has_one :schedule
+  has_one :preference
+  has_and_belongs_to_many :skills, :join_table => 'users_skills'
   
   ##
   # Class methods
   ##
-
+  ##
+  # Class methods
+  ##
+  def self.from_omniauth(auth)
+    new do |profile|
+      profile.first_name = auth[:info][:first_name]
+      profile.last_name = auth[:info][:last_name]
+      if auth[:provider] == 'linkedin'
+        profile.photo = auth[:extra][:raw_info][:pictureUrls][:values].first
+      else
+        profile.photo = auth[:info][:image]
+      end
+    end
+  end
+  
+  ##
+  # Instance methods
+  ##  
+  def add_skills_from_str(p)
+    str = p[:skills]
+    list = str.split(/\,/).map {|s| s.downcase.strip}
+    list.each do |s|
+      unless Skill.exists?(:name => s)
+        self.skills.create!(:name => s)
+      else
+        self.skills << Skill.find_by!(:name => s) unless self.skills.include?(Skill.find_by(:name => s))
+      end
+    end      
+  end
+  
   ##
   # Instance methods
   ##
--- test/factories/users.rb
+++ test/factories/users.rb
@@ -8,5 +8,13 @@ FactoryGirl.define do
     bio { Faker::Lorem.sentences.join(' ') }
     remember_token { SecureRandom.uuid }    
     photo { "http://api.randomuser.me/portraits/" + %w{ men/ women/ }.shuffle.first + Random.new.rand(1..80).to_s + ".jpg" }
+    preference
+    
+    factory :seed_user do
+      after(:build) do |user|
+        user.courses << build(:course, :instructor => user)
+        user.skills = build_list(:skill, Random.new.rand(3..6))
+      end
+    end
   end
 end
